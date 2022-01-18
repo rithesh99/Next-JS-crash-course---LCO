4d50df96312ab7806320dedbcbe3ad8daa432805
rithesh99 <ritheshdavid@gmail.com>
2022-01-18 10:57:43 +0530

    DB connection

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI: "mongodb+srv://rithesh:rithesh07@ecommerce.n5jp9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  }
 }
